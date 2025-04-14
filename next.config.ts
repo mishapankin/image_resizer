86b9bed69f8dbe74703bb573003cbfa9fa79f3e1
Mikhail <mishapankin@gmail.com>
2025-04-15 00:32:24 +0300

    GH pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? "/image_resizer" : "",
+  assetPrefix: isProd ? "/image_resizer/" : "",
+  output: "export",
 };
 
 export default nextConfig;
